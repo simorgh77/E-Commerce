24974ae3f23df3a8addbd6d9a74f664990bcc21e
simorgh <tahakhorasani77@gmail.com>
2021-08-01 00:38:23 -0700

    setup app

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React ,{useEffect}from 'react';
+import  Header  from './components/Header/Header';
+import Mynavbar from './components/Mynavbar/Mynavbar';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+import './App.css';
+import routes from './routes/main.routes';
+import { RouteComponentProps } from "react-router";
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Footer from './components/Footer/Footer';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+      <Header >
+      <Mynavbar/>
+      </Header>
+<Switch>
+      {routes.map(item=>(
+
+<Route  path={item.path} exact={item.exact}  render={props=><item.page {...props}/>}                   />
+
+      ))}
+</Switch>
+
+<div className="footer">
+<Footer/>
+</div>
+</Router>
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "swiper/swiper.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {makeServer} from './server/server'
+// import { Provider } from 'react-redux';
+// import store from './store/store';
+if(process.env.NODE_ENV==="development"){
+  makeServer({environment:'development'})
+}
+ReactDOM.render(
+  <React.StrictMode>
+   
+    <App />
+   
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
