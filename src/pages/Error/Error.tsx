a00c830d7f4ea759999b5c6e8f78a83245f73647
simorgh <tahakhorasani77@gmail.com>
2021-08-01 00:43:59 -0700

    create error page

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import error from "../../assest/img/error.svg"
+const Error = () => {
+    return (
+        <div className="w-100  mx-auto">
+        <div className="w-100  mx-auto">
+            <img  src={error} alt="error" style={{fontSize:"10rem"}}/>
+         
+        </div>
+        </div>
+    )
+}
+
+export default Error
