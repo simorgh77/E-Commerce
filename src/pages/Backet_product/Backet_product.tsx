5d99c75a2da6dd37b60e3109f4513e87286aaabb
simorgh <tahakhorasani77@gmail.com>
2021-08-01 00:42:34 -0700

    initial basket_page

---
+++ src/pages/Backet_product/Backet_product.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Backet_product = () => {
+    return (
+        <div>
+            this is bascket page
+        </div>
+    )
+}
+
+export default Backet_product
