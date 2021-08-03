4415c747db6a42f01b420becd41d6c79fe6c6af4
simorgh <tahakhorasani77@gmail.com>
2021-08-03 00:41:50 -0700

    create footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,83 +1,74 @@
 import React from 'react'
-import {InputGroup,FormControl,Button} from "react-bootstrap"
-import { FaInstagram ,FaTwitter ,FaLinkedin} from "react-icons/fa";
-const 
+import { Form, FormControl, Button, ListGroup } from "react-bootstrap"
+import { FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { Container, Row, Col } from 'react-bootstrap'
+import { BsChevronCompactLeft } from "react-icons/bs";
+import "./footer.style.css"
+const Footer = () => {
+  return (
+    <footer className='d-flex flex-column w-100' style={{ backgroundColor: '#2b3445' }}>
 
-Footer = () => {
-    return (
-        <footer className='d-flex flex-column'  style={{backgroundColor:'#2b3445'}}>
-        <div 
-        className="d-flex justify-content-around ">
+
+      <Container className='w-100 h-100'>
+        <Row className='w-100 mx-auto d-flex text-white'>
+        <Col xs={12} md={12} style={{fontSize:'110%'}} 
+        className='  text-center w-100 d-flex flex-column justify-content-between '>
+          <Col xs={9} md={12} className=' mb-3 mx-auto'>{"مارا در شبکه های اجتماعی دنبال کنید"}</Col>
+          <Col xs={3} md={12} className='w-100 d-flex my-auto mx-auto justify-content-around' 
+          style={{fontSize:'2rem'}} ><FaInstagram/> <FaTwitter/> <FaLinkedin/> </Col>
+        </Col>
+
+
+        <Col  md={12} className='mt-5 d-flex flex-wrap'>
+          <Col xs={6} md={12} className='d-flex flex-wrap'>
             
-<div className="list d-flex justify-content-around mt-5 w-75" >
-<ul className="text-center text-white" style={{ listStyleType: "none"}}>
-<h1>sdf</h1>
-  <li className="">An item</li>
-  <li className="">A second item</li>
-  <li className="">A third item</li>
-  <li className="">A fourth item</li>
-  <li className="">And a fifth one</li>
-</ul>
-<ul className="text-center text-white" style={{ listStyleType: "none"}}>
-<h1>sdf</h1>
-  <li className="">An item</li>
-  <li className="">A second item</li>
-  <li className="">A third item</li>
-  <li className="">A fourth item</li>
-  <li className="">And a fifth one</li>
-</ul>
-<ul className="text-center text-white" style={{ listStyleType: "none"}}>
-<h1>sdf</h1>
-  <li className="">An item</li>
-  <li className="">A second item</li>
-  <li className="">A third item</li>
-  <li className="">A fourth item</li>
-  <li className="">And a fifth one</li>
-</ul>
-<ul className="text-center text-white" style={{ listStyleType: "none"}}>
-<h1>sdf</h1>
-  <li className="">An item</li>
-  <li className="">A second item</li>
-  <li className="">A third item</li>
-  <li className="">A fourth item</li>
-  <li className="">And a fifth one</li>
-</ul>
-<ul className="text-center text-white" style={{ listStyleType: "none"}}>
-<h1>sdf</h1>
-  <li className="">An item</li>
-  <li className="">A second item</li>
-  <li className="">A third item</li>
-  <li className="">A fourth item</li>
-  <li className="">And a fifth one</li>
-</ul>
-</div>
-<div className="icons my-auto h-50">
-    <h2 className='text-white d-flex w-100 justify-content-around '> مارا دنبال کنید</h2>
-            <FaInstagram style={{fill:"lightgray",fontSize:"2.5rem"}} />
-            <FaTwitter style={{fill:"lightgray",fontSize:"2.5rem"}}/>
-            <FaLinkedin style={{fill:"lightgray",fontSize:"2.5rem"}}/>
-            </div>
-        </div>
-<div className="email mx-auto ">
-    <h4 className="text-white mb-3 text-center">{"از تخفیفات ما با خبر شوید"}</h4>
-<InputGroup className="mb-3 w-100 d-flex justify-contetn-around">
-    <FormControl
-      placeholder="ایمیل خود را وارد نمایید"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-    <Button id="button-addon2" className='bg-success text-dark rounded'>
-     {"ثبت"}
-    </Button>
-  </InputGroup>
+            <ul className='d-flex flex-wrap  w-100 justify-content-between'>
+            <li className=''>{" پشتیبانی مشتریان "}</li>
+            <li className=''>{"سوالات متداول"}</li>
+            <li className=''>{"شرایط اقساطی"}</li>
+            <li className=''>{"شرایط مرجوعی"}</li>
+            </ul></Col>
+          <Col xs={6} md={12} className='d-flex flex-wrap '>
 
-</div>
+          <ul className='d-flex flex-wrap w-100 justify-content-between '>
+            <li className=''>{"شرایط استفاده"}</li>
+            <li className=''>{"حریم خصوصی"}</li>
+            <li className=''>{"درباره ما"}</li>
+            <li className=''>{"تماس با ما"}</li>
+            </ul>
+          </Col>
+        </Col>
 
-        <div className="copyright text-white w-100 bg-dark text-center p-3 sticky-bottom">
-            کلیه حقوق سایت متعلق به طه خراسانی میباشد
-        </div>
-        </footer>
-    )
+
+
+
+        <Col md={12} className='d-flex'>
+           <Col md={4} className='text-center'><img className=' ' src="https://www.keshmoon.com/images/template/footer/follow-up-icon.svg" alt="" /> <span>{"پیگیری سفارش"}</span></Col>
+           <Col md={4} className='text-center'><img className=' ' src="https://www.keshmoon.com/images/template/footer/shipping-icon.svg" alt="" /> <span>{"اطلاعات ارسال"}</span></Col>
+           <Col md={4} className='text-center'><img className=' ' src="https://www.keshmoon.com/images/template/footer/return-icon.svg" alt="" /> <span>{"اطلاعات مرجوعی"}</span></Col>
+        </Col>
+        <Col  md={12} className='d-flex flex-column justify-content-center mt-5'>
+<Col md={6}></Col>
+          <Form>
+  <Form.Group className="mb-3 w-75 mx-auto" controlId="formBasicEmail">
+    <Form.Control type="email" className='text-center' placeholder="برای پیگیری اخبار ایمیل خود را وارد نمایید" />
+  </Form.Group>
+  </Form>
+          <input type="button" value="ثبت ایمیل" className='btn btn-success  mx-auto ' />
+        </Col>
+
+        </Row>
+      </Container>
+
+
+
+
+
+      <div className="copyright text-white w-100 bg-dark text-center p-3 sticky-bottom">
+        کلیه حقوق سایت متعلق به طه خراسانی میباشد
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
