599ef4218995367e4684973ac472002182bc7c8f
simorgh <tahakhorasani77@gmail.com>
2021-08-10 22:53:34 -0700

    create shopping page

--- src/pages/Shopping_page/Shopping_page.tsx
+++ src/pages/Shopping_page/Shopping_page.tsx
@@ -1,10 +1,114 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { BsArrowRight } from "react-icons/bs";
+import { Col,Container,Row, Form,Button} from 'react-bootstrap'
+import { GrStar } from "react-icons/gr";
 const Shopping_page = () => {
     return (
-        <div>
-            this is shoping page
-        </div>
+        <Container className='bg-white rounded ' style={{boxShadow:'5px 5px 15px 5px rgba(0,0,0,0.3)'}}>
+
+            <Row className=' m-0 mt-5 d-flex flex-wrap flex-column'>
+<h2 className='text-black mt-4'>{"افزودن آدرس جدید"}</h2>
+
+<div className='border-bottom '>
+                <Col  md={12} className=' d-flex flex-wrap'>
+
+                <Col xs={12} md={6} className='px-4'>
+
+  <Form.Select className="mt-5 "  aria-label="Default select example">
+  <option>{'استان را مشخص کنید'}</option>
+  <option value="1">{"کرمان"}</option>
+  <option value="2">{"تهران"}</option>
+  <option value="3">{"مشهد"}</option>
+  </Form.Select>
+ 
+
+                </Col>
+
+                <Col xs={12} md={6} className='px-4'>
+            
+                <Form.Select className="mt-5  "  aria-label="Default select example">
+                  
+  <option>{'شهر را مشخص کنید'}</option>
+  <option value="1">{"رفسنحان"}</option>
+  <option value="2">{"سیرجان"}</option>
+  <option value="3">{"بم"}</option>
+  </Form.Select>
+ 
+
+                </Col>
+
+                </Col>
+                    
+
+
+                <Col  md={12} className='d-flex flex-wrap '>
+
+                <Col xs={12} md={6} className='mt-5 px-4'>
+
+  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+    <Form.Label>{'نشانی پستی'}</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+
+                    
+</Col>
+<Col xs={12} md={6} className='mt-5 px-4'>
+<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+<Form.Label>{'کد پستی'}</Form.Label>
+<Form.Control type="text" />
+
+</Form.Group>               
+</Col> 
+                </Col> 
+                
+               
+
+
+
+
+
+
+
+                <Col  md={6} className='d-flex  justify-content-between '>
+
+                <Col xs={6} md={3} className='d-flex flex-wrap px-4 w-50'>
+                <Form.Group className="mb-3 " controlId="exampleForm.ControlTextarea1">
+<Form.Label>{'پلاک'}</Form.Label>
+<Form.Control type="text" />
+
+</Form.Group>           
+                    
+</Col>
+<Col xs={6} md={3} className='d-flex flex-wrap px-4 w-50'>
+<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+<Form.Label>{'واحد'}</Form.Label>
+<Form.Control type="text" />
+
+</Form.Group>           
+                    
+                </Col>
+                </Col>
+                </div>
+
+
+                <Col  md={6} className='mt-4 px-4'>
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+<Form.Label>{'نام و نام خانوادگی گیرنده '}</Form.Label>
+<Form.Control type="text" />
+
+</Form.Group>    
+                </Col>
+                <Button style={{backgroundColor:'#56B68B',border:'none'}} variant="primary" className='my-4' size="lg">
+    {"ادامه خرید"}
+  </Button>
+
+  <Link className='mb-4' to='/basket' > <BsArrowRight style={{fontSize:'2rem'}}/> {"بازگشت به سبد خرید"} </Link>
+            </Row>
+
+
+        </Container>
     )
 }
 
