3809ee80b17cf921d711d4cea2fa86f5aebee205
simorgh <tahakhorasani77@gmail.com>
2021-08-15 07:49:54 -0700

    change product detail page style and add some scroll spy for information

--- src/pages/Product_Details/Product_Details.tsx
+++ src/pages/Product_Details/Product_Details.tsx
@@ -12,6 +12,7 @@ import { TypedUseSelectorHook} from 'react-redux'
 import {RootState,AppDispatch} from '../../store/store'
 import { AiOutlineCheck } from "react-icons/ai";
 import Humanize from "humanize-plus"
+import Scrollspy from 'react-scrollspy'
 
 interface IProducts{
     id: string,
@@ -75,8 +76,10 @@ Singleproduct &&dispatch(AddTobasket(Singleproduct))
 
 }
 
-    return (
-        <div className='mt-5'>
+
+return (
+  
+  <div className='mt-5 ' >
              <div className='header text-center pt-4 text-white '>
                 <p>{Singleproduct?.name}</p>
              </div>
@@ -85,8 +88,16 @@ Singleproduct &&dispatch(AddTobasket(Singleproduct))
             <Col md={12} className='bg-white d-flex 
             flex-wrap justify-content-between first-col'>
               <Col xs={12} md={4}>
-
+              <div id='badge-ribbon' className='off text-center text-white d-flex justify-content-center align-items-center'
+                  style={{fontSize:'1.5rem',position:'absolute'}} >
+                   
+                
+                   {'%'+Singleproduct?.offpercent} 
+                   <br />
+                   {'حراج'}
+                   </div>
    
+                 
     <img
       className="d-block w-100"
       src={Singleproduct?.image}
@@ -105,56 +116,61 @@ Singleproduct &&dispatch(AddTobasket(Singleproduct))
               </Col>
 
 
-              <Col xs={12} md={4} className='d-flex flex-column justify-content-around' >
-              <Form.Select className='w-50 mx-auto' aria-label="Default select example">
-  <option>{"انتخاب سایز"}</option>
-  <option value="sm">sm</option>
-  <option value="md">md</option>
-  <option value="lg">lg</option>
-  <option value="xlg">xlg</option>
-  <option value="xxlg">xxlg</option>
-</Form.Select>
-              <Form.Select className='w-50 mt-2 mx-auto' aria-label="Default select example">
-  <option>{"تعداد"}</option>
-  <option value="1">1</option>
-  <option value="2">2</option>
-  <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="5">5</option>
-</Form.Select>
-<div className='d-flex flex-column mx-auto mt-2'>
-    <div className='mx-auto'>{'رنگ'}</div>
-    <div className='d-flex '>
-<Form.Check type="radio" name='color' style={{border:'red'}} aria-label="radio 1" />
-<Form.Check type="radio" name='color' style={{color:'red'}} aria-label="radio 2" />
-<Form.Check type="radio" name='color' style={{color:'red'}} aria-label="radio 3" />
-<Form.Check type="radio" name='color' style={{color:'red'}} aria-label="radio 4" />
+     
+
+
+
+
+              <Col xs={12} md={6} className='buy_card justify-content-around  rounded mt-2  m-0 d-flex flex-column '>
+<div className='name' style={{fontWeight:'bolder',fontSize:'20px'}}>{Singleproduct?.name}</div>
+<div className='description '>
+{Singleproduct?.description}
+</div>
+               <div className='price mt-2'>
+   
+                 <div style={{color:'gray',textDecoration:'line-through'
+                 ,fontSize:'18px'}}>
+                   {Humanize.intComma(parseInt(Singleproduct?.price as 
+                    string))} {'تومان'}</div>
+<div
+className='w-100 d-flex flex-column mt-2' style={{fontSize:'40px',color:'#F04D7D'}}>
+  <span>
+  {Humanize.intComma(parseInt(Singleproduct?.price as string)-(parseInt(Singleproduct?.price as string)*(Singleproduct?.offpercent as number)/100))}{"تومان"}
+  </span> 
 </div>
 </div>
-              </Col>
+  <span className='text-dark mt-2' style={{fontSize:'25px'}}>
+  <BsStarFill fill='rgb(254,165,105)'/> {Singleproduct?.rate} 
+  </span>
+    
+  <div className=''>
+  {"انتخاب سایز:"} <Form.Select size="sm" className='' style={{width:'25vw'}}>
+    <option disabled={true}>{'انتخاب سایز'}</option>
+    <option >{'S'}</option>
+    <option >{' M'}</option>
+    <option >{'L'}</option>
+    <option >{'XL'}</option>
+    <option >{'XXL'}</option>
+  </Form.Select></div>
 
-
-
-
-              <Col xs={12} md={4} className='buy_card rounded mt-2  m-0 d-flex flex-column justify-content-around'>
-                 <div className='d-flex justify-content-between'>{"فروشنده"} 
-                 <div> {Singleproduct?.rate} <BsStarFill fill='rgb(254,165,105)'/></div></div>
-                 <div>{"قنبری و دوستان"}</div>
-                 <div>{"گارانتی اصالت و سلامت فیزیکی کالا"}</div>
-                 <div>{" 40 درصد رضایت خریداران"}</div>
-                 <div>{"وضعیت انبار :"}{Singleproduct?.isexist}</div>
-                 <div style={{color:'red',textDecoration:'line-through',fontSize:'15px'}}>{Humanize.intComma(parseInt(Singleproduct?.price as string))} {'تومان'}</div>
 
 
                { !Buybutton?<div><Button  onClick={handle_add_to_bascket}
-                 className='w-100'style={{backgroundColor:"rgb(239,57,78)",border:'none'}}>
+                 className='w-100 mt-3'style={{backgroundColor:"rgb(239,57,78)",border:'none'}}>
                      {"افزودن به سبد خرید"}</Button></div>: 
                      <p className='text-center' style={{color:'green'}  }>
                        {"به سبد خرید اضافه شد"}<AiOutlineCheck/>
                      </p> 
                     
                      }
-              </Col>
+                     <div className='gurantee  d-flex my-3 w-100'>
+                       <div className='w-50 d-flex align-items-center text-center flex-column flex-wrap mx-auto' style={{color:'gray',fontSize:'15px'}}><img className='img-fluid' src="https://www.digistyle.com/static/files/bc93f765.svg" width='20%'  alt="" />{'ضمانت  چهارده روز بازگشت کالا'}</div>
+                       <div className='w-50 d-flex align-items-center text-center flex-column flex-wrap mx-auto' style={{color:'gray',fontSize:'15px'}}><img className='img-fluid' src="https://www.digistyle.com/static/files/c968eac8.svg"  width='20%' alt="" />{"تحویل سریع و آسان"}</div>
+                       <div className='w-50 d-flex align-items-center text-center flex-column flex-wrap mx-auto' style={{color:'gray',fontSize:'14px'}}><img className='img-fluid' src="https://www.digistyle.com/static/files/bafc4f4a.svg"  width='20% flex-wrap' alt="" />{"ضمانت اصل بودن کالا"}</div>
+                     </div>
+
+                    
+      </Col>
                 </Col>
              
              <Col md={12} className='mt-5'> 
@@ -169,9 +185,47 @@ Singleproduct &&dispatch(AddTobasket(Singleproduct))
              
              </Col>
 
+<Col md={12} className='bg-white '>
 
-
-             <Col md={12} className='d-flex flex-wrap'>
+<div className='w-100'>
+<Row>
+<Scrollspy 
+        className="scrollspy d-flex  m-0 p-0 sticky-top"   items={ ['section-1', 'section-2', 'section-3', 'section-4', 'section-5'] } 
+        currentClassName="isCurrent">
+        <li><a href="#section-1">{'مشخصات محصول'}</a></li>
+        <li><a href="#section-2"> {'دیدگاه کاربران'}</a></li>
+      
+      </Scrollspy>
+        <section id="section-1">
+          <h2 className='mt-3'>{"مشخصات محصول"}</h2>
+          <p className='text-end px-3' style={{color:'gray',fontSize:'20px'}}>{'مشخصات'}</p>
+          <div className='d-flex listofinfo' >
+          <ul className='m-0 text-end bold'>
+            <li>{"جنس"}</li>
+            <li>{"قد"}</li>
+            <li>{"یقه"}</li>
+            <li>{"آستین"}</li>
+            <li>{"طرح"}</li>
+            <li>{"توضیحات جنس"}</li>
+            <li>{"کشور تولید کننده"}</li>
+          </ul>
+          <ul>
+            <li>{"یشمی"}</li>
+            <li>{"تا زانو"}</li>
+            <li>{"برگردان"}</li>
+            <li>{"کوتاه"}</li>
+            <li>{"ساده"}</li>
+            <li>{"تهیه شده از پشم ها گوسفند"}</li>
+            <li>{"ایران"}</li>
+          </ul>
+          </div>
+          </section>
+        
+      
+        <section id="section-2">
+          <h2>{"دیدگاه کاربران"}</h2>
+          
+          <Col md={12} className='d-flex flex-wrap'>
              <Col xs={12} md={6} className='bg-white'>
                  <div className='text-center'>{"افزودن دیدگاه"}</div>
                  <Form className='mt-3 w-75  mx-auto' >
@@ -214,6 +268,17 @@ Singleproduct &&dispatch(AddTobasket(Singleproduct))
                   <div className="review mt-2 px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque rerum quisquam natus doloribus dolore qui consequuntur, voluptatum aliquid voluptas laborum architecto ipsum ipsa, repudiandae, iure veniam aliquam magnam soluta. Necessitatibus.</div>
              </Col>
              </Col>
+        </section>
+      </Row>
+      </div>
+     
+
+</Col>
+
+
+
+
+
              </Row>
          </Container>
         </div>
