cf5dfcc9cff5d3fe90ba06792fe16c38442c5804
simorgh <tahakhorasani77@gmail.com>
2021-08-16 04:02:49 -0700

    create navbar

--- src/components/Mynavbar/Mynavbar.tsx
+++ src/components/Mynavbar/Mynavbar.tsx
@@ -1,77 +1,113 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Nav,DropdownButton,Dropdown,NavDropdown} from 'react-bootstrap'
 import "./Mynavbar.style.css"
+import { useHistory } from 'react-router'
+import { render } from '@testing-library/react'
 const Mynavbar = () => {
+const history=useHistory()
+
+  const [newestShow,setnewestShow]=useState(false)
+  const [manShow,setmanShow]=useState(false)
+  const [womanShow,setwomanShow]=useState(false)
+  const [kidShow,setkidShow]=useState(false)
+  const [babyShow,setbabyShow]=useState(false)
+  const[rerender,setrerender]=useState(false)
     return (
-    //   <div className="dropdown">
-    //   <button className="">Dropdown</button>
-    //   <div className="dropdown-content">
-    //     <a href="#">Link 1</a>
-    //     <a href="#">Link 2</a>
-    //     <a href="#">Link 3</a>
-    //   </div>
-    // </div>
+
         <div>
             <Nav >
   <Nav.Item className="dropdown">
-  <NavDropdown id="dropdown-button dropdown" title="جدید ترین ها" className="btn">
-  <Dropdown.Item href="#/action-1">کیف</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">کفش</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">ساعت</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">دامن</Dropdown.Item>
+  <NavDropdown id="dropdown-button dropdown"
+   title="جدید ترین ها" className="btn hover" show={newestShow}
+   onMouseEnter={()=>setnewestShow(true)}
+   onMouseLeave={()=>setnewestShow(false)} >
+  <Dropdown.Item onClick={()=>history.push
+    ('/Product_Categoriespants?category=tag&varient=newest')} >
+    شلوار</Dropdown.Item>
+  <Dropdown.Item onClick={()=>history.push
+    ('/Product_Categoriesshirt?category=tag&varient=newest')}  >پیراهن</Dropdown.Item>
+  <Dropdown.Item onClick={()=>history.push
+    ('/Product_Categoriesshoes?category=tag&varient=newest')} >کفش</Dropdown.Item>
+  <Dropdown.Item onClick={()=>history.push
+    ('/Product_Categoriesbag?category=tag&varient=newest')}
+   >کیف</Dropdown.Item>
 </NavDropdown>
   </Nav.Item>
 
-  
   <Nav.Item>
-  <NavDropdown id="dropdown-button" title="مردانه" className="btn">
-  <Dropdown.Item href="#/action-1">کیف</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">کفش</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">ساعت</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">دامن</Dropdown.Item>
-</NavDropdown>
-  </Nav.Item>
-
-
-  <Nav.Item>
-  <NavDropdown id="dropdown-button" title="زنانه" className="btn">
-  <Dropdown.Item href="#/action-1">کیف</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">کفش</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">ساعت</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">دامن</Dropdown.Item>
-</NavDropdown>
-  </Nav.Item>
-
-
-
-  <Nav.Item>
-    <NavDropdown id="dropdown-button" title="بچگانه" className="btn">
-  <Dropdown.Item href="#/action-1">کیف</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">کفش</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">ساعت</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">دامن</Dropdown.Item>
+  <NavDropdown id="dropdown-button" title="مردانه" className="btn hover" 
+   show={manShow}
+   onMouseEnter={()=>setmanShow(true)}
+   onMouseLeave={()=>setmanShow(false)}>
+  <Dropdown.Item 
+  onClick={()=>{history.push
+    ('/Product_Categoriesman?category=category&varient=pants');setrerender(!render)}}>شلوار</Dropdown.Item>
+  <Dropdown.Item 
+  onClick={()=>{history.push
+    ('/Product_Categoriesman?category=category&varient=shirt');setrerender(!render)}}>پیراهن</Dropdown.Item>
+  <Dropdown.Item 
+  onClick={()=>{history.push
+    ('/Product_Categoriesman?category=category&varient=shoes');setrerender(!render)}}>کفش</Dropdown.Item>
+  <Dropdown.Item 
+  onClick={()=>{history.push
+    ('/Product_Categoriesman?category=category&varient=bag');setrerender(!render)}}>کیف</Dropdown.Item>
 </NavDropdown>
   </Nav.Item>
 
 
   <Nav.Item>
-    <NavDropdown id="dropdown-button" title="نوزاد" className="btn">
-  <Dropdown.Item href="#/action-1">کیف</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">کفش</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">ساعت</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">دامن</Dropdown.Item>
+  <NavDropdown id="dropdown-button" title="زنانه" className="btn hover" 
+   show={womanShow}
+   onMouseEnter={()=>setwomanShow(true)}
+   onMouseLeave={()=>setwomanShow(false)}>
+  <Dropdown.Item   onClick={()=>{history.push
+    ('/Product_Categorieswoman?category=category&varient=pants');Mynavbar()}}>شلوار</Dropdown.Item>
+  <Dropdown.Item   onClick={()=>{history.push
+    ('/Product_Categorieswoman?category=category&varient=shirt');Mynavbar()}}>پیراهن</Dropdown.Item>
+  <Dropdown.Item   onClick={()=>history.push
+    ('/Product_Categorieswoman?category=category&varient=shoes')}>کفش</Dropdown.Item>
+  <Dropdown.Item   onClick={()=>history.push
+    ('/Product_Categorieswoman?category=category&varient=bag')}>کیف</Dropdown.Item>
 </NavDropdown>
   </Nav.Item>
+
 
 
   <Nav.Item>
-     <NavDropdown id="dropdown-button" title="برند ها" className="btn">
-  <Dropdown.Item href="#/action-1">nike</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">puma</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">ساعت</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">دامن</Dropdown.Item>
+    <NavDropdown id="dropdown-button" title="بچگانه" className="btn hover" 
+     show={kidShow}
+     onMouseEnter={()=>setkidShow(true)}
+     onMouseLeave={()=>setkidShow(false)}>
+  <Dropdown.Item onClick={()=>history.push
+    ('/Product_Categorieskid?category=category&varient=pants')}>شلوار</Dropdown.Item>
+  <Dropdown.Item onClick={()=>history.push
+    ('/Product_Categorieskid?category=category&varient=shirt')}>پیراهن</Dropdown.Item>
+  <Dropdown.Item onClick={()=>history.push
+    ('/Product_Categorieskid?category=category&varient=shoes')}>کفش</Dropdown.Item>
+  <Dropdown.Item onClick={()=>history.push
+    ('/Product_Categorieskid?category=category&varient=bag')}>کیف</Dropdown.Item>
 </NavDropdown>
   </Nav.Item>
+
+
+  <Nav.Item >
+    <NavDropdown id="dropdown-button" title="نوزاد" className="btn hover" 
+     show={babyShow}
+     onMouseEnter={()=>setbabyShow(true)}
+     onMouseLeave={()=>setbabyShow(false)} >
+  <Dropdown.Item  onClick={()=>history.push
+    ('/Product_Categoriesbaby?category=category&varient=pants')}>شلوار</Dropdown.Item>
+  <Dropdown.Item  onClick={()=>history.push
+    ('/Product_Categoriesbaby?category=category&varient=shirt')}>پیراهن</Dropdown.Item>
+  <Dropdown.Item  onClick={()=>history.push
+    ('/Product_Categoriesbaby?category=category&varient=shoes')}>کفش</Dropdown.Item>
+  <Dropdown.Item  onClick={()=>history.push
+    ('/Product_Categoriesbaby?category=category&varient=bag')}>کیف</Dropdown.Item>
+</NavDropdown>
+  </Nav.Item>
+
+
+ 
 
   
 </Nav>
